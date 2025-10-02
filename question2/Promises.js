1b6fbaada8d3ebb42735d7f57961d9f867f010d4
Iznmav <andreijhosephgania@gmail.com>
2025-10-02 16:40:07 -0400

    question2

---
+++ question2/Promises.js
@@ -0,0 +1,23 @@
+const resolvedPromise = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ message: 'delayed success!' });
+    }, 500);
+  });
+};
+
+const rejectedPromise = () => {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject({ error: 'delayed exception!' });
+    }, 500);
+  });
+};
+
+resolvedPromise()
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+rejectedPromise()
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
